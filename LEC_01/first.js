2d988dbc2f70a4d0d829fea6f999df5ad32bd99c
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-11-18 11:10:21 +0530

    Lec 1

---
+++ LEC_01/first.js
@@ -0,0 +1,18 @@
+// console.log("Hello World");
+
+name = "Tony Stark";
+
+age = 25;
+price = 150
+
+x = null;
+u = undefined;
+
+isFollow = false;
+
+console.log(name);
+console.log(price);
+console.log(age);
+console.log(x);
+console.log(u);
+console.log(isFollow);
