e7568f92abd9e90d54f42b97bc6739e641df0b2d
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-11-18 11:23:59 +0530

    LEC 01

--- LEC_01/first.js
+++ LEC_01/first.js
@@ -1,18 +1,43 @@
 // console.log("Hello World");
 
-name = "Tony Stark";
+// name = "Tony Stark";
 
-age = 25;
-price = 150
+// age = 25;
+// price = 150
 
-x = null;
-u = undefined;
+// x = null;
+// u = undefined;
 
-isFollow = false;
+// isFollow = false;
 
-console.log(name);
-console.log(price);
-console.log(age);
-console.log(x);
-console.log(u);
-console.log(isFollow);
+// console.log(name);
+// console.log(price);
+// console.log(age);
+// console.log(x);
+// console.log(u);
+// console.log(isFollow);
+
+
+// Q1
+// ANS:-
+
+const product = {
+    name: "Pen ",
+    type: "BAll",
+    rating: 4.5,
+    price: 20,
+};
+
+console.log(product);
+
+// Q2 ANS:-
+
+const profile = {
+    name: "Shardha Khapra",
+    username: "shardhakhapra",
+    posts:"56",
+    follwers:"520",
+    following:"15",
+    bio:"Apna College"
+}
+console.log(profile);
