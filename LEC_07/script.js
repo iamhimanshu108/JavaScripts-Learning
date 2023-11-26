ed2aeff74876d561b793f97bf2e4ce1056ab7d50
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-11-26 17:14:41 +0530

    DOM

---
+++ LEC_07/script.js
@@ -0,0 +1,16 @@
+// Q1
+
+// let newBtn = document.createElement("button")
+// newBtn.innerText = "clickMe";
+// newBtn.style.color = "white";
+// newBtn.style.backgroundColor = "red";
+
+// document.querySelector("body").prepend(newBtn)
+
+
+// Q2
+
+
+let para = document.querySelector("p")
+
+para.getAttribute("class")
