40018ac0713844eb20dcdfdace42d7548f50e69b
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-03-06 19:32:55 +0530

    Operators and Conditionals

--- LEC_02/script.js
+++ LEC_02/script.js
@@ -1,1 +1,132 @@
-console.log("Hello");
+// console.log("Operators & Conditionlas ");
+
+// Arthimetic Operators
+/** 
+let a = 5;
+let b = 2;
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+
+*/
+
+// UnarY Operator
+/*
+let a = 5;
+let b = 2;
+
+console.log("a + b = ", a + b);
+a++; 
+console.log("a = ", a);
+a--;
+console.log("a = ", a);
+++a;
+console.log("++a = ", a);
+--a;
+console.log("--a = ", a);
+*/
+
+
+// Asigmnet Operators
+/*
+let a = 5;
+let b = 2;
+
+a += 4; // a = a+4  - * / % sb use kr skte hai......
+
+console.log("a = ",a);
+*/
+
+// Comparision Operators
+/*
+let a = 5;
+let b = 2;
+
+console.log("5 == 2", a==b);
+console.log("5 == a", a==a);
+*/
+
+// Logical Operator
+/*
+let a = 5;
+let b = 2;
+
+let cond1 = a > b; //true
+let cond2 = a ===6; // true
+console.log("cond1 && cond2 = ", cond1 && cond2);
+
+*/
+
+//Conditional Statements
+
+// if statement
+/*
+let age = 25;
+
+if(age> 18){
+    console.log("You can Vote");
+}
+if(age < 18){
+    console.log("You cannot Vote");
+}
+
+let mode  = "dark"
+let color;
+
+if(mode == "dark"){
+    color = "balck"
+}
+if(mode == "dark"){
+    color = "white"
+}
+
+console.log(color);
+
+*/
+
+// odd Or Even
+/*
+
+let num  = 19;
+
+if(num % 2 === 0){
+    console.log(num, "is even");
+}else {
+    console.log(num, "is odd");
+}
+*/
+
+// syntax -> Rules
+/*
+let mode = "dark";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+} else if(mode === "blue"){
+    color = "blue"
+} else if(mode === "pink"){
+    color: "pink";
+} else {
+    color: "white"
+}
+
+console.log(color);
+
+if (mode === "dark") {
+    console.log(mode);
+}
+*/
+
+// ternary 
+
+let age = 25;
+
+let result  = age>=18 ? "adult" : "Not adult";
+
+console.log(result);
+
