2c1d14441fb0ef70f56f95f781dc43c0e55aea30
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2024-03-12 18:05:07 +0530

    Classes

--- LEC_12/app.js
+++ LEC_12/app.js
@@ -1,3 +1,19 @@
-console.log("one");
-console.log("Two");
-console.log("Three");
+// console.log("one");
+// console.log("Two");
+// console.log("Three");
+
+/* Method 1
+
+function hello() {
+    console.log("Hello");
+}
+
+setTimeout(hello, 5000);
+*/
+
+
+// method 2
+
+setTimeout(()=>{
+    console.log("Hello");
+}, 5000);
