bd3be12bec30a38556286a859e4991dc31a13ffc
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2024-03-20 19:11:55 +0530

    Promise

---
+++ Revision/promise.js
@@ -0,0 +1,20 @@
+function createPromise(){
+    return new Promise(function exec(resolve, reject){
+        setTimeout(function f(){
+            console.log("timer done");
+            resolve("done")
+        },3000)
+    })
+}
+
+console.log("start");
+
+let x = createPromise();
+
+console.log("got a new promise");
+
+x.then(function f(){
+    console.log("promise done");
+})
+
+console.log("end");
