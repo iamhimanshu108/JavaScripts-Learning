565f6c6008023c1a3309c74b71b7ff15fc7009f9
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-04-01 17:40:55 +0530

    Conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,12 @@
 // let score = "Himanshu123"
 
-// // console.log(typeof score);
-// // console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
+
 
 // let valueInNumber = Number(score)
-// // console.log(typeof valueInNumber);
-// // console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // // "33" => 33
@@ -15,22 +16,22 @@
 // let isLoggedIn = "Himanshu"
 
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
-// //  console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// // 1 => true; 0 => false
-// // "" => false
-// // "hitesh" => true
+// // // 1 => true; 0 => false
+// // // "" => false
+// // // "hitesh" => true
 
 // let someNumber = 33
 
 // let stringNumber = String(someNumber)
-// // console.log(stringNumber);
-// // console.log(typeof stringNumber); 
+// console.log(stringNumber);
+// console.log(typeof stringNumber); 
 
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+// let value = 3
+// let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
